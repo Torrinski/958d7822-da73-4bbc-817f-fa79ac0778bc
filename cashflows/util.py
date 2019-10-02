e940b1db5f57ffbea23bfbfb818e34dda295b29f
DE LA TORRE JIMENEZ O'FARRIL <if705733@iteso.mx>
2019-10-01 21:14:58 -0500

    Archivo util Cashflow

--- cashflows/util.py
+++ cashflows/util.py
@@ -2,20 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+"Creating a Cashflow class, with attributes and methods:"
 class Cashflow(object):
-    """Cashflow
-    Create a cashflow-class definition.
-    
-    Attributes: 
-        * amount - monetary amount at time t.
-        * t - integer representing time. 
-        
-    Methods:
-        * present_value(self, interest_rate) - returns the present value of the cashfow given a interest-rate.
-    """
 
+    def __init__(self, amount, t):
+        self.amount = amount
+        self.t = t
 
+    def present_value (self, interest_rate):
+        return self.amount / (1+interest_rate)**self.t
+
+"Implementing the plot method on the invenstment porject class and plotting on main class"
 class InvestmentProject(object):
     RISK_FREE_RATE = 0.08
 
@@ -36,33 +33,36 @@ class InvestmentProject(object):
     def internal_return_rate(self):
         return np.irr([flow.amount for flow in self.cashflows])
 
-    def plot(self, show=False):
-        """Plot Cashflows
-        The `plot` function creates a bar plot (fig) where x=t and y=amount.
-        :param show: boolean that represents whether to run `plt.show()` or not.
-        :return: matplotlib figure object.
-        """
-        # TODO: implement plot method
-        raise NotImplementedError
+    def plot(self, show=False,**kwargs):
+
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
 
 
+        self.figure=self.ax.plot([flow.t for flow in self.cashflows],[flow.amount for flow in self.cashflows])
+        if show:
+            plt.show()
+        if kwargs.get('name'):
+            plt.title('cashflows')
+            plt.ylabel('amount')
+            plt.xlabel('t')
+            self.fig.savefig(kwargs.get('name'))
+
+"Implementing Net present value method"
     def net_present_value(self, interest_rate=None):
-        """ Net Present Value
-        Calculate the net-present value of a list of cashflows.
-        :param interest_rate: represents the discount rate.
-        :return: a number (currency) representing the net-present value.
-        """
-        # TODO: implement net_present_value method
-        raise NotImplementedError
+        return sum([flow.present_value(interest_rate=interest_rate) for flow in self.cashflows]) if not interest_rate==None else sum([flow.present_value(interest_rate=self.hurdle_rate) for flow in self.cashflows])
 
+
+"Defining the equivalent annuity"
     def equivalent_annuity(self, interest_rate=None):
         """ Equivalent Annuity
         Transform a set of cashflows into a constant payment.
         :param interest_rate: represents the interest-rate used with the annuity calculations.
         :return: a number (currency) representing the equivalent annuity.
         """
+        return self.net_present_value(interest_rate=interest_rate)
         # TODO: implement equivalent_annuity methdo
-        raise NotImplementedError
+        #raise NotImplementedError
 
     def describe(self):
         return {
@@ -71,3 +71,8 @@ class InvestmentProject(object):
             "net-present-value": self.net_present_value(interest_rate=None),
             "equivalent-annuity": self.equivalent_annuity(interest_rate=None)
         }
+cashflows=[Cashflow(100,1),Cashflow(100,2),Cashflow(100,3)]
+investmentP=InvestmentProject(cashflows)
+#investmentP.plot(name='test.png')
+print(investmentP.net_present_value())
+#plt.show()
